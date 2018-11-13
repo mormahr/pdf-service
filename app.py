dd4cbe7d0916d03786766ca4eb5f139857471de3
Moritz Mahringer <me@moritzmahringer.de>
2018-11-13 02:41:34 +0100

    Initial Version

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, make_response
+from weasyprint import HTML
+
+app = Flask(__name__)
+
+
+@app.route('/generate', methods=['POST'])
+def make_pdf():
+    data = request.get_data(as_text=True)
+    print(data)
+    html = HTML(string=data)
+    doc = html.render()
+    pdf = doc.write_pdf()
+
+    response = make_response(pdf)
+
+    response.headers.set('Content-Type', 'application/pdf')
+    response.headers.set('Content-Disposition', 'attachment; filename="generated.pdf"')
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
