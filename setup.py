6d2842a44841cc3360715ea571f0c2aceb0ebaa2
Moritz Mahringer <me@moritzmahringer.de>
2018-11-13 03:14:09 +0100

    Rename

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='flaskr',
+    name='pdf-service',
     version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
