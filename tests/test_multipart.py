a729ded6c8f47c0e8ec000cc4ad478dbd2a7a9d8
Moritz Mahringer <me@moritzmahringer.de>
2021-03-01 14:00:21 +0100

    Add test for relative urls and fix incorrect test name

--- tests/test_multipart.py
+++ tests/test_multipart.py
@@ -40,10 +40,28 @@ def test_contains_text(client):
     assert 'Test text in PDF' in text
 
 
-def test_renders_external_resource(client: Client):
+def test_renders_absolute_internal_resource(client: Client):
     data = {
         'index.html': (
             BytesIO(bytes('<p>Test <img src="/test.png"/></p>', 'utf8')),
+            'index.html',
+            'text/html'),
+        'test.png': (
+            Path(__file__).parent.joinpath('../test-data/assets/test.png').open('rb'),
+            'test.png',
+            'image/png'
+        )
+    }
+
+    rv = client.post('/generate', data=data)
+    assert 200 == rv.status_code
+    assert 'application/pdf' == rv.content_type
+
+
+def test_renders_relative_internal_resource(client: Client):
+    data = {
+        'index.html': (
+            BytesIO(bytes('<p>Test <img src="test.png"/></p>', 'utf8')),
             'index.html',
             'text/html'),
         'test.png': (
