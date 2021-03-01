19fc46806a40b56d7cf02cacf656e8efa0921fbf
Moritz Mahringer <me@moritzmahringer.de>
2021-03-01 01:04:44 +0100

    Refactor in preparation of merge with multipart handler
    Use BytesIO instead of text string for HTML

--- pdf_service/generate_basic.py
+++ pdf_service/generate_basic.py
@@ -1,3 +1,5 @@
+from io import BytesIO
+
 from flask import make_response, Response, request
 from sentry_sdk import start_span, set_context
 from weasyprint import HTML
@@ -7,11 +9,11 @@ from .URLFetchHandler import URLFetchHandler
 
 def generate_basic() -> Response:
     with start_span(op='decode'):
-        data = request.get_data(as_text=True)
+        html_file = BytesIO(request.get_data())
 
     with URLFetchHandler(request.files) as url_fetcher:
         with start_span(op='parse'):
-            html = HTML(string=data, url_fetcher=url_fetcher)
+            html = HTML(file_obj=html_file, url_fetcher=url_fetcher)
 
         with start_span(op='render'):
             doc = html.render()
@@ -20,7 +22,7 @@ def generate_basic() -> Response:
         pdf = doc.write_pdf()
 
     set_context("pdf-details", {
-        "html_size": len(data),
+        "html_size": html_file.getbuffer().nbytes,
         "pdf_size": len(pdf),
     })
 
